2f53125dad6dcb91ecefa9cb56cba41af5b5e7c8
saini302005 <sanjusaini999012345@gmail.com>
2026-03-30 14:55:42 +0530

    Time: 1 ms (43.91%) | Memory: 42.9 MB (40.34%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String sortSentence(String s) {
+        String[] words = s.split(" ");
+        String[] result = new String[words.length];
+        for(String word : words){
+            int pos = word.charAt(word.length() - 1)-'0';
+            result[pos - 1] = word.substring(0, word.length() - 1);
+        }
+        return String.join(" ", result);
+    }
+}
