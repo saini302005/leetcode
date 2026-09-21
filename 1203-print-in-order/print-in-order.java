52357fbf1c1d82e53a1fd40eb4f1a6d2167c8d35
saini302005 <sanjusaini999012345@gmail.com>
2026-09-21 16:57:21 +0530

    Time: 13 ms (11.79%) | Memory: 43.9 MB (87.78%) - LeetSync

--- 1203-print-in-order/print-in-order.java
+++ 1203-print-in-order/print-in-order.java
@@ -1,31 +1,39 @@
 class Foo {
+    private Object lock;
     private boolean oneDone;
     private boolean twoDone;
     
     public Foo() {
+        lock = new Object();
         oneDone = false;
         twoDone = false;
     }
 
-    public synchronized void first(Runnable printFirst) throws InterruptedException {
-        printFirst.run();
-        oneDone = true;
-        notifyAll();
+    public void first(Runnable printFirst) throws InterruptedException {
+        synchronized (lock) {
+            printFirst.run();
+            oneDone = true;
+            lock.notifyAll();
+        }
     }
 
-    public synchronized void second(Runnable printSecond) throws InterruptedException {
-        while (!oneDone) {
-            wait();
+    public void second(Runnable printSecond) throws InterruptedException {
+        synchronized (lock) {
+            while (!oneDone) {
+                lock.wait();
+            }
+            printSecond.run();
+            twoDone = true;
+            lock.notifyAll();
         }
-        printSecond.run();
-        twoDone = true;
-        notifyAll();
     }
 
-    public synchronized void third(Runnable printThird) throws InterruptedException {
-        while (!twoDone) {
-            wait();
+    public void third(Runnable printThird) throws InterruptedException {
+        synchronized (lock) {
+            while (!twoDone) {
+                lock.wait();
+            }
+            printThird.run();
         }
-        printThird.run();
     }
 }
